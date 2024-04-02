c2efba20fbe6cc4b85c2582e11d98b5c4fe7e4e3
Jivan Taj <TAJJA1@ETSU.EDU>
2024-04-02 19:38:16 -0400

    image gen

---
+++ HennovativeTechnologies/HennovativeTechnologies/Controllers/ImageGenController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HennovativeTechnologies.Models;
+using System.Diagnostics;
+
+namespace HennovativeTechnologies.Controllers
+{
+    public class ImageGenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
