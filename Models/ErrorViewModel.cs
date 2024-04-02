ba60d7c5eab2cd0be759aed90692e55403e8574f
nhorner9 <hornern@etsu.edu>
2024-04-02 17:16:34 -0400

    Fixed Repo

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HennovativeTechnologies.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
